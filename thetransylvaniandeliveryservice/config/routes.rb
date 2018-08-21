a009e213b052cfa89e394aa38776ff8ad0b9a991
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-08-21 01:45:34 +0000

    4.4 Models & Associations

--- thetransylvaniandeliveryservice/config/routes.rb
+++ thetransylvaniandeliveryservice/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'simple_pages#index'
+  resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'simple_pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'simple_pages#landing_page'
 end
+
--- thetransylvaniandeliveryservice/db/seeds.rb
+++ thetransylvaniandeliveryservice/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "seed-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
